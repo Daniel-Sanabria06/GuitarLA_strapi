f7dffecd753b81bf44bfa27716a06a3c1c83a04f
Daniel-Sanabria06 <80917159+Daniel-Sanabria06@users.noreply.github.com>
2022-07-17 16:43:43 -0500

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '30590bc799177b3565229d2285924eb3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7776252d-54c1-485c-a2e5-2550f621d25b'
+};
